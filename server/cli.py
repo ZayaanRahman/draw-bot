41c48172b1faf2da9f685357fa2b4bf23da4fa1c
Zayaan Rahman <126630377+ZayaanRahman@users.noreply.github.com>
2024-05-16 01:33:10 -0500

    Implemented server and inheritance relationship

--- server/cli.py
+++ server/cli.py
@@ -1,25 +1,97 @@
 from controller import Controller
 
-class Cli:
-    
+
+class Cli(Controller):
+
     def __init__(self, pi_ip, pi_port):
-        self.controller = Controller(pi_ip, pi_port)
-        
+
+        super().__init__(pi_ip, pi_port)
+        # mapping of command name to function
+        self.commands = {
+            "help": self.help,
+            "status": self.status,
+            "add": self.add,
+            "start": self.start,
+            "end": self.end,
+            "exit": self.exit
+        }
+
     def run(self):
         print("\nWelcome 🤖!")
         print("Type \"help\" for a list of commands.\n")
-        while not self.controller.end_signal:
+        while not self.end_signal:
             output = self.process_input(input("[🤖?] << "))
             print("[🤖!] >> " + output + "\n")
-        
+
     # interpret user input
     def process_input(self, input):
 
         arg_list = input.split(" ")
         command = arg_list[0].strip()
 
-        if not command in self.controller.commands:
+        if not command in self.commands:
             return "invalid command"
         else:
-            return self.controller.commands[command](arg_list[1:])
-        
+            return self.commands[command](arg_list[1:])
+
+    # list commands and functionality
+    def help(self, args):
+        output = "commands:\n"
+        output += "=================================\n"
+        output += "help: list commands\n"
+        output += "status: view robot state\n"
+        output += "add: append points to robot itinerary\n"
+        output += "start: start robot movement\n"
+        output += "end: end robot movement\n"
+        output += "exit: exit controller\n"
+        output += "================================="
+        return output
+
+    def status(self, args):
+
+        # parse
+        state_dict = super().status()
+        output = f"robot state at {state_dict["time"]}:\n"
+        output += "=================================\n"
+        output += f"status: {state_dict["status"]}\n"
+        output += f"position: {state_dict["position"]}\n"
+        output += f"visited: {state_dict["visited"]}\n"
+        output += f"to visit: {state_dict["to visit"]}\n"
+        output += "================================="
+        return output
+
+    # to add 3 points, type args like this: a,b c,d f,g
+    def add(self, args):
+
+        new_pts = []
+
+        for point in args:
+            arr = point.split(",")
+            if not len(arr) == 2:
+                return {"error": "incorrect format, do \"a,b c,d e,f\" for points"}
+
+            x = self.float_conv(arr[0])
+            y = self.float_conv(arr[1])
+
+            if x == None or y == None:
+                return {"error": "incorrect format, do \"a,b c,d e,f\" for points"}
+
+            new_pts.append([x, y])
+
+        new_queue = super().add(new_pts)["queue"]
+        tuple_q = []
+        for item in new_queue:
+            tuple_q.append(tuple(item))
+        return f"updated queue: {tuple_q}"
+
+    def start(self, args):
+        return super().start()["message"]
+
+    def end(self, args):
+        return super().end()["message"]
+
+    # exit controller, ending robot operation and controller
+
+    def exit(self, args):
+        self.end_signal = True
+        return "exiting..👋"
--- server/controller.py
+++ server/controller.py
@@ -12,86 +12,34 @@ class Controller:
 
         self.pi_address = "http://" + pi_ip + ":" + pi_port + "/"
 
-        # mapping of command name to function
-        self.commands = {
-            "help": self.help,
-            "status": self.status,
-            "add": self.add,
-            "start": self.start,
-            "end": self.end,
-            "exit": self.exit
-        }
-
-        self.end_signal = False
-        self.is_started = False
-
-    # --CLI COMMANDS-----------------------------------------------------------------------------------
+    # --BASE COMMANDS----------------------------------------------------------------------------------
     # -------------------------------------------------------------------------------------------------
 
-    # list commands and functionality
-    def help(self, args):
-        output = "commands:\n"
-        output += "=================================\n"
-        output += "help: list commands\n"
-        output += "status: view robot state\n"
-        output += "add: append points to robot itinerary\n"
-        output += "start: start robot movement\n"
-        output += "end: end robot movement\n"
-        output += "exit: exit controller\n"
-        output += "================================="
-        return output
-
     # get current robot status
-    def status(self, args):
-        # print(self.pi_address+"get_status")
+    def status(self):
         response = requests.get(self.pi_address + "get_status")
-        return response.json()["status"]
+        return response.json()
 
     # add a point or points to robot queue
-    # to add 3 points, type args like this: a,b c,d f,g
-    def add(self, args):
-
-        new_pts = []
-
-        for point in args:
-            arr = point.split(",")
-            if not len(arr) == 2:
-                return "incorrect format, do \"a,b c,d e,f\" for points"
-
-            x = self.float_conv(arr[0])
-            y = self.float_conv(arr[1])
-
-            if x == None or y == None:
-                return "incorrect format, do \"a,b c,d e,f\" for points"
-
-            new_pts.append([x, y])
+    def add(self, new_points):
 
         headers = {'Content-Type': 'application/json'}
-        json_data = json.dumps({"points": new_pts})
+        json_data = json.dumps({"points": new_points})
 
         response = requests.put(
             self.pi_address + "add_points", data=json_data, headers=headers)
-        tuple_q = []
-        for item in response.json()["queue"]:
-            tuple_q.append(tuple(item))
-
-        return "updated queue: " + str(tuple_q)
+        
+        return response.json()
 
     # start robot operation
-
-    def start(self, args):
+    def start(self):
         response = requests.post(self.pi_address + "start_run")
-        return response.json()["message"]
+        return response.json()
 
     # end robot operation
-    def end(self, args):
+    def end(self):
         response = requests.post(self.pi_address + "end_run")
-        return response.json()["message"]
-
-    # exit controller, ending robot operation and controller
-    def exit(self, args):
-        self.end_signal = True
-        return "exiting..👋"
+        return response.json()
 
     # --HELPER FUNCTIONS-------------------------------------------------------------------------------
     # -------------------------------------------------------------------------------------------------
